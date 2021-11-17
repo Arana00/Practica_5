b4bfba5b884db8ddc4f772a3f73efa4e78475678
Celiasf <celia.sanchez4@isepceu.es>
2021-11-17 13:50:50 +0100

    Comprobación de contraseña e input de guardar añadido.

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-    session_start();
     //Función 1 para que salga un mensaje en caso de que se introduzcan números en vez de letras en los campos de nombre,
     //apellido y ciudad. Con expresión regular.
     function nom($string)
@@ -18,18 +17,27 @@
 
     $provincias = [];
     $string = file_get_contents('cpProvincias.csv');
-    $provincias_cod = explode("\n", $string);
-    foreach ($provincias_cod as $fila) {
-        $separar = explode(";", $fila);
+    $provincias_cod = explode("\n",$string);
+    foreach($provincias_cod as $fila){
+        $separar = explode(";",$fila);
         $provincias[] = [
             'codigo' => $separar[0],
             'provincia' => $separar[1]
         ];
-
-
     }
-    print_r($provincias[0]['codigo'] . $provincias[0]['provincia'] . '<br />');
-    print_r($provincias);
+
+    //CON EL IF COMPRUEBO QUE HE ENVIADO EL FORMULARIO
+    if(isset($_POST['guardar'])) {
+        //recojo los valores
+        $contrasenia = filter_input(INPUT_POST, 'i', FILTER_SANITIZE_STRING);
+    }
+    //Expresión para comprobar los carácteres de la contraseña
+    $regex = "/((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9])).{8,16}/";
+    //If que compara la contraseña insertada con mi expresión regular, si la contraseña esta insertada y esta no cumple
+    //los patrones me devuelve un mensaje de que es incorrecta.
+    if ($contrasenia != "" && preg_match($regex, $contrasenia) != 1) {
+        $mensaje = "La contraseña no cumple las normas, inserte una nueva";
+    }
 
     ?>
     <!DOCTYPE html>
@@ -48,17 +56,19 @@
                     <label for="c">Dirección:</label> <input name="c" id="c" type="text" placeholder="C. Alustante, 13"><br/>
                     <label for="d">Ciudad:</label> <input name="d" id="d" type="text" placeholder="Madrid"><br/>
                     <label for="e">Provincia:</label>
-                    <select name="select">
+                    <select name="select" >
                         <option hidden selected>Selecciona una provincia</option>
-                        <?php foreach ($provincias as $provincia) { ?>
-                            <option value="value1"><?php echo $provincia['provincia'] ?> </option>
+                        <?php foreach ($provincias as $provincia){?>
+                            <option value="value1"><?php echo $provincia['provincia']?> </option>
                         <?php } ?>
                     </select>
                     <label for="f">Código postal:</label> <input name="f" id="f" type="text" placeholder="28002"><br/>
                     <label for="g">Teléfono:</label> <input name="g" id="g" type="tel" placeholder="123-45-67-89"><br/>
                     <label for="h">Email:</label> <input name="h" id="h" type="email" placeholder="ejemplo@gmail.com">
                     <label for="i">Contraseña:</label> <input name="i" id="i" type="password" placeholder="**********">
+                    <?php echo $mensaje; ?>
                     <label for="j">Web:</label> <input name="j" id="j" type="url" placeholder="www.holamundo.com">
+                    <input class="boton" type="submit" name="guardar" value="Guardar">
                 </fieldset>
             </form>
 
